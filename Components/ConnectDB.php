97985fd94612a4c2cd46f78f5d7f66c1f186bf79
mmmaimankarae <mineteacha@hotmail.com>
2024-03-23 23:19:20 +0700

    add header

--- Components/ConnectDB.php
+++ Components/ConnectDB.php
@@ -1,3 +1,19 @@
 <?php
-    $connectDB = mysqli_connect("localhost", "root", "", "myStore");
+  /* Connect to the database แบบ Procedural */
+//   $connectDB = mysqli_connect("localhost", "root", "", "myStore");
+  
+  /* Specify connection */
+  $servername = "localhost:3306";
+  $username = "root";
+  $password = "rootroot";
+  $dbname = "user_jwt";
+
+  /* Create connection แบบ OOP */
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  /* Check connection */
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  } else {
+    echo "Connected successfully";
+  }
 ?>
