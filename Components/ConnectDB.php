c4723e65a8cf1a35faf7ca48b9aa661e42b08d55
TitlePnp <pphanupong.tle@gmail.com>
2024-03-24 01:57:04 +0700

    edit  connectedDB

--- Components/ConnectDB.php
+++ Components/ConnectDB.php
@@ -17,17 +17,17 @@ if ($conn->connect_error) {
 //   $connectDB = mysqli_connect("localhost", "root", "", "myStore");
   
   /* Specify connection */
-  $servername = "localhost:3306";
-  $username = "root";
-  $password = "rootroot";
-  $dbname = "user_jwt";
+  // $servername = "localhost:3306";
+  // $username = "root";
+  // $password = "rootroot";
+  // $dbname = "user_jwt";
 
-  /* Create connection แบบ OOP */
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  /* Check connection */
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  } else {
-    echo "Connected successfully";
-  }
+  // /* Create connection แบบ OOP */
+  // $conn = new mysqli($servername, $username, $password, $dbname);
+  // /* Check connection */
+  // if ($conn->connect_error) {
+  //   die("Connection failed: " . $conn->connect_error);
+  // } else {
+  //   echo "Connected successfully";
+  // }
 ?>
---
+++ Frontend/MainPage/Home.php
@@ -0,0 +1,32 @@
+<?php
+require '../../Components/HeaderUser.html';
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kodchasan:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+    <title>SMITI SHOP: HOME</title>
+    
+    <style>
+        * {
+            font-family: Kodchasan;
+        }
+    </style>
+
+<body>
+    <div class="flex flex-col justify-center items-center h-screen">
+        <h1 class="text-3xl font-bold">This Page Under Maintain!!🔨</h1>
+
+    </div>
+</body>
+
+</html>
