fa17a8245cec73da4175b082711074e340e83d73
TitlePnp <pphanupong.tle@gmail.com>
2024-03-27 14:26:39 +0700

    Add Sing Out

---
+++ Frontend/MainPage/SummaryOrder.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kodchasan:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+    <title>SMITI SHOP:Summary Order</title>
+    <style>
+        * {
+            font-family: 'Kodchasan';
+            margin: 0px;
+            padding: 0px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="flex flex-row px-28 pt-8">
+        <div class="w-full bg-red-500">
+            <h1 class="text-lg">จัดส่งที่</h1>
+        </div>
+        <div class="w-3/6 bg-green-500">
+            <p>test</p>
+        </div>
+    </div>
+</body>
+
+</html>
