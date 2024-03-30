d768e3c87e2619342bfd5683e11459b091a48843
TitlePnp <pphanupong.tle@gmail.com>
2024-03-30 20:22:19 +0700

    add UserProfile

--- Frontend/Profile/UserProfile.php
+++ Frontend/Profile/UserProfile.php
@@ -1,3 +1,13 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SMITI:User Profile</title>
+</head>
+<body>
+    <div>
+        
+    </div>
+</body>
+</html>
