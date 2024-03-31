0ba5bd36a91b273c7f7c61705d4790ec336b451c
mmmaimankarae <mineteacha@hotmail.com>
2024-03-31 11:01:59 +0700

    optimaize admin authorized, add require no guest

---
+++ Backend/Authorized/NoGuestAuthorized.php
@@ -0,0 +1,34 @@
+<?php
+  session_start();
+  require '../../Backend/Authorized/GoogleIdRole.php';
+  require '../../vendor/autoload.php';
+  use Firebase\JWT\Key;
+  use \Firebase\JWT\JWT;
+
+  $key = "SECRETKEY_SMITIECOM_CLIENT";
+
+  if (isset($_SESSION['tokenJWT'])) {
+    $jwt = $_SESSION['tokenJWT'];
+    $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+    $role = $decoded->role;
+    if ($role == "Admin") {
+      header('Location: ../../Frontend/admin/admin.php');
+      exit();
+    } elseif ($role == "SuperAdmin") {
+      header('Location: ../../Frontend/admin/Test.php');
+      exit();
+    }
+  } elseif (isset($_SESSION['tokenGoogle'])) {
+    $role = getRole($_SESSION['tokenGoogle']);
+    if ($role == "Admin") {
+      header('Location: ../../Frontend/admin/Test.php');
+      exit();
+    } elseif ($role == "SuperAdmin") {
+      header('Location: ../../Frontend/admin/Test.php');
+      exit();
+    }
+  } else {
+      header('Location: ../../Frontend/MainPage/Home.php');
+      exit();
+  }
+?>
