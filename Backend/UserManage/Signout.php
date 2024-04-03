51cea63ee56c2d194925eef2b9eda4f7558aba87
TitlePnp <pphanupong.tle@gmail.com>
2024-04-04 00:33:49 +0700

    Add log Signout

--- Backend/UserManage/Signout.php
+++ Backend/UserManage/Signout.php
@@ -1,5 +1,16 @@
 <?php
 session_start();
+require_once "../Log/LogManage.php";
+date_default_timezone_set('Asia/Bangkok');
+if (isset($_SESSION['tokenJWT'])) {
+    insertLog("JWT Logout", date("Y-m-d H:i:s"));
+
+} else if (isset($_SESSION['tokenGoogle'])) {
+    insertLog("Google Logout Google", date("Y-m-d H:i:s"));
+} else {
+    insertLog("Loginout Role? ", date("Y-m-d H:i:s"));
+}
+
 session_unset();
 session_destroy();
 header("Location: ../../Frontend/SignIn_Page/SignIn.php");
